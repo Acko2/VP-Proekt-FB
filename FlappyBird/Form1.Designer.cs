2db6ab085467c39deef7417c4aa0460382c64f89
Aleksandar Alimaskoski <63553277+Acko2@users.noreply.github.com>
2022-06-28 17:22:32 +0200

    Add files via upload

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.LStart = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pipeTimer = new System.Windows.Forms.Timer(this.components);
+            this.pbBg = new System.Windows.Forms.PictureBox();
+            this.ScoreText = new System.Windows.Forms.Label();
+            this.lReset = new System.Windows.Forms.Label();
+            this.GameOver = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameOver)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.Image = global::FlappyBird.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-12, 544);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(810, 50);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.BackColor = System.Drawing.Color.Transparent;
+            this.flappyBird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.flappyBird.Image = global::FlappyBird.Properties.Resources.bird;
+            this.flappyBird.Location = new System.Drawing.Point(143, 263);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(43, 24);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 4;
+            this.flappyBird.TabStop = false;
+            // 
+            // LStart
+            // 
+            this.LStart.AutoSize = true;
+            this.LStart.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LStart.Location = new System.Drawing.Point(216, 254);
+            this.LStart.Name = "LStart";
+            this.LStart.Size = new System.Drawing.Size(495, 49);
+            this.LStart.TabIndex = 5;
+            this.LStart.Text = "Press \"Space\" to start";
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.Location = new System.Drawing.Point(379, 13);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(47, 44);
+            this.Score.TabIndex = 6;
+            this.Score.Text = "0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.game_tick);
+            // 
+            // pipeTimer
+            // 
+            this.pipeTimer.Interval = 3500;
+            this.pipeTimer.Tick += new System.EventHandler(this.pipe_tick);
+            // 
+            // pbBg
+            // 
+            this.pbBg.BackColor = System.Drawing.Color.LemonChiffon;
+            this.pbBg.Location = new System.Drawing.Point(161, 88);
+            this.pbBg.Name = "pbBg";
+            this.pbBg.Size = new System.Drawing.Size(481, 319);
+            this.pbBg.TabIndex = 7;
+            this.pbBg.TabStop = false;
+            // 
+            // ScoreText
+            // 
+            this.ScoreText.AutoSize = true;
+            this.ScoreText.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ScoreText.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreText.Location = new System.Drawing.Point(271, 270);
+            this.ScoreText.Name = "ScoreText";
+            this.ScoreText.Size = new System.Drawing.Size(109, 33);
+            this.ScoreText.TabIndex = 8;
+            this.ScoreText.Text = "label1";
+            // 
+            // lReset
+            // 
+            this.lReset.AutoSize = true;
+            this.lReset.BackColor = System.Drawing.Color.LemonChiffon;
+            this.lReset.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lReset.Location = new System.Drawing.Point(219, 336);
+            this.lReset.Name = "lReset";
+            this.lReset.Size = new System.Drawing.Size(369, 33);
+            this.lReset.TabIndex = 9;
+            this.lReset.Text = "Press \"Enter\" to restart";
+            // 
+            // GameOver
+            // 
+            this.GameOver.BackColor = System.Drawing.Color.LemonChiffon;
+            this.GameOver.Image = global::FlappyBird.Properties.Resources.GameOver;
+            this.GameOver.Location = new System.Drawing.Point(253, 151);
+            this.GameOver.Name = "GameOver";
+            this.GameOver.Size = new System.Drawing.Size(289, 56);
+            this.GameOver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GameOver.TabIndex = 10;
+            this.GameOver.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(781, 583);
+            this.Controls.Add(this.GameOver);
+            this.Controls.Add(this.lReset);
+            this.Controls.Add(this.ScoreText);
+            this.Controls.Add(this.pbBg);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.LStart);
+            this.Name = "Form1";
+            this.Text = "FlappyBird";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.onPress);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameOver)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.Label LStart;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer pipeTimer;
+        private System.Windows.Forms.PictureBox pbBg;
+        private System.Windows.Forms.Label ScoreText;
+        private System.Windows.Forms.Label lReset;
+        private System.Windows.Forms.PictureBox GameOver;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int g = 8;
+        int f;
+        int s = 4;
+        int score = 0;
+        static Random tmp = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LStart.Visible = true;
+            Score.Visible = false;
+            pbBg.Visible = false;
+            GameOver.Visible = false;
+            ScoreText.Visible = false;
+            lReset.Visible = false;
+            firstPipes();
+        }
+        //timer za sozdavanje na cevki
+        private void pipe_tick(object sender, EventArgs e)
+        {
+            mPipes();
+        }
+        //timer za igrata
+        private void game_tick(object sender, EventArgs e)
+        {
+            //dvizenje na cevkite vo levo
+            foreach(Control i in this.Controls)
+            {
+                if(i is PictureBox && i.Tag == "pipeTop")
+                {
+                    i.Left -= s;
+                    //brisenje na cevki
+                    if(((PictureBox)i).Left < this.Width - 950)
+                    {
+                        this.Controls.Remove(i);
+                    }
+                    
+                    //proveruva dali pticata udira vo gornata cevka
+                    if (i.Bounds.IntersectsWith(flappyBird.Bounds))
+                    {
+                        gameEnd();
+                    }
+                }
+            }
+            //dvizenje na cevkite vo levo
+            foreach (Control j in this.Controls)
+            {
+                if (j is PictureBox && j.Tag == "pipeBottom")
+                {
+                    j.Left -= s;
+                    //brisenje na cevki
+                    if (((PictureBox)j).Left < this.Width - 950)
+                    {
+                        this.Controls.Remove(j);
+                    }
+                    //proveruva dali pticata udira vo dolnata cevka
+                    if (j.Bounds.IntersectsWith(flappyBird.Bounds))
+                    {
+                        gameEnd();
+                    }
+
+                }
+            }
+            //dvizenje na transparenten picturebox za poeni
+            foreach(Control k in this.Controls)
+            {
+                if(k is PictureBox && k.Tag == "points")
+                {
+                    k.Left -= s;
+                    if (k.Bounds.IntersectsWith(flappyBird.Bounds))
+                    {
+                        score++;
+                        this.Controls.Remove(k);
+                        Speed_Increase(score);
+                    }
+
+                }
+            }
+            //proveruva dali pticata udira vo zemjata
+            if (flappyBird.Bounds.IntersectsWith(ground.Bounds))
+            {
+                gameEnd();
+            }
+            //ogranicuvanje na preletuvanje nad formata
+            if(flappyBird.Top < this.Height - 615)
+            {
+                flappyBird.Location = new Point(143, 8);
+            }
+
+            //skokanje na pticata
+            flappyBird.Top -= f;
+            if(f > -8)
+            {
+                f -= 1;
+            }
+            else
+            {
+                f = -8;
+            }
+            Score.Text = score.ToString();
+        }
+        //pravenje cevki
+        public void mPipes()
+        {
+            PictureBox pipeTop = new PictureBox();
+            PictureBox pipeBottom = new PictureBox();
+            PictureBox points = new PictureBox();
+            pipeTop.Image = Properties.Resources.pipedown;
+            pipeBottom.Image = Properties.Resources.pipe;
+            pipeTop.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeBottom.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeTop.Size = new Size(100, 450);
+            pipeBottom.Size = new Size(100, 450);
+            points.Size = new Size(2, 622);
+            int value = tmp.Next(-380, -200);
+            pipeTop.Location = new Point(800, value);
+            pipeBottom.Location = new Point(800, 600 + value);
+            points.Location = new Point(885, 0);
+            pipeTop.Tag = "pipeTop";
+            pipeBottom.Tag = "pipeBottom";
+            points.Tag = "points";
+            this.Controls.Add(pipeTop);
+            this.Controls.Add(pipeBottom);
+            this.Controls.Add(points);
+        }
+        //funkcija za prvite cevki za da se gledaat na formata
+        public void firstPipes()
+        {
+            PictureBox pipeTop = new PictureBox();
+            PictureBox pipeBottom = new PictureBox();
+            PictureBox points = new PictureBox();
+            pipeTop.Image = Properties.Resources.pipedown;
+            pipeBottom.Image = Properties.Resources.pipe;
+            pipeTop.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeBottom.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeTop.Size = new Size(100, 450);
+            pipeBottom.Size = new Size(100, 450);
+            points.Size = new Size(2, 622);
+            int value = tmp.Next(-380, -200);
+            pipeTop.Location = new Point(750, value);
+            pipeBottom.Location = new Point(750, 600 + value);
+            points.Location = new Point(835, 0);
+            pipeTop.Tag = "pipeTop";
+            pipeBottom.Tag = "pipeBottom";
+            points.Tag = "points";
+            this.Controls.Add(pipeTop);
+            this.Controls.Add(pipeBottom);
+            this.Controls.Add(points);
+        }
+
+        
+        private void onPress(object sender, KeyEventArgs e)
+        {
+            //skokanje i pocetok na igrata
+            if(e.KeyCode == Keys.Space)
+            {
+                if (!gameTimer.Enabled && pbBg.Visible == false)
+                {
+                    gameTimer.Start();
+                    pipeTimer.Start();
+                    f = g;
+                    LStart.Visible = false;
+                    Score.Visible = true;
+                }
+
+                f = g;
+            }
+            
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(!gameTimer.Enabled && pbBg.Visible == true)
+                {
+                    reset();
+                }
+            }
+
+           
+        }
+        //zgolemuvanje na brzinata na dvizenje na cevkite za pogolema tezina na igrata
+        private void Speed_Increase(int score)
+        {
+            if (score == 10)
+            {
+                s += 1;
+                pipeTimer.Interval = 2500;
+            }
+            if (score == 25)
+            {
+                s += 2;
+                pipeTimer.Interval = 1800;
+            }
+            if (score == 50)
+            {
+                s += 4;
+                pipeTimer.Interval = 1300;
+            }
+            if (score == 100)
+            {
+                s += 6;
+                pipeTimer.Interval = 800;
+            }
+
+        }
+        //funkcija za restartiranje na igrata
+        private void reset()
+        {
+            gameTimer.Stop();
+            pipeTimer.Stop();
+
+            //brisenje na cevkite posle restart
+            for(int b = this.Controls.Count -1; b >= 0; b--)
+            {
+                if(this.Controls[b] is PictureBox && this.Controls[b].Tag == "pipeTop")
+                {
+                    this.Controls[b].Dispose();
+                }
+                else if(this.Controls[b] is PictureBox && this.Controls[b].Tag == "pipeBottom")
+                {
+                    this.Controls[b].Dispose();
+                }
+                else if(this.Controls[b] is PictureBox && this.Controls[b].Tag == "points")
+                {
+                    this.Controls[b].Dispose();
+                }
+            }
+
+            flappyBird.Location = new Point(143, 263);
+            score = 0;
+            s = 4;
+            f = g;
+            pbBg.Visible = false;
+            GameOver.Visible = false;
+            lReset.Visible = false;
+            ScoreText.Visible = false;
+            LStart.Visible = true;
+            pipeTimer.Interval = 3500;
+            firstPipes();
+            
+        }
+        //funkcija za kraj na igrata
+        private void gameEnd()
+        {
+            gameTimer.Stop();
+            pipeTimer.Stop();
+            Score.Visible = false;
+            pbBg.Visible = true;
+            GameOver.Visible = true;
+            ScoreText.Text = "Your score is: " + score;
+            ScoreText.Visible = true;
+            lReset.Visible = true;
+        }
+    }
+}
